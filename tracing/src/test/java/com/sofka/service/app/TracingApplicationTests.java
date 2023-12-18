38414a641e93233ed395fad11b3d896d530fbf0a
Ricardo Carvajal <rcarvajal@intelix.biz>
2023-12-18 17:29:35 -0400

    type(feat): creating structure ----------------------------------------- Detailed description:
    BREAKING CHANGE:creating structure of
    project
    -----------------------------------------

---
+++ tracing/src/test/java/com/sofka/service/app/TracingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.service.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
