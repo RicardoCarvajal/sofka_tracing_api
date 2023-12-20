d4d7164e54b6599ec6e1fcd31b90f17e8d7b18cb
Ricardo Carvajal <rcarvajal@intelix.biz>
2023-12-20 01:02:50 -0400

    type(refactor): cleaning project ----------------------------------------- Detailed description:
    refactor!: cleaning class project
    unnecessary
    -----------------------------------------

--- tracing/src/main/java/com/sofka/service/app/infraestructure/drivenAdapter/repository/IMovimientoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sofka.service.app.infraestructure.drivenAdapter.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.sofka.service.app.infraestructure.drivenAdapter.entity.Movimiento;
-
-public interface IMovimientoRepository extends ReactiveMongoRepository<Movimiento, String> {
-
-}
